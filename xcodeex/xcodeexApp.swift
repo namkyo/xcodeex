698de6a39f408b48ea819d9d90648d36cf4c5bc0
namkyo <hipen1@naver.com>
2021-01-10 00:43:50 +0900

    Initial Commit

---
+++ xcodeex/xcodeexApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeexApp.swift
+//  xcodeex
+//
+//  Created by 김남교 on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct xcodeexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
